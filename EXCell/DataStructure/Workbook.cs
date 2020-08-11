d0effb418fb48d716d5523dc0f9aad24a793c542
BanalityOfSeeking <rapril@specpage.com>
2020-08-11 09:03:23 -0400

    Anonymize

--- EXCell/DataStructure/Workbook.cs
+++ EXCell/DataStructure/Workbook.cs
@@ -1,18 +1,4 @@
-﻿// ***********************************************************************
-// Assembly         : SSDMGMT.Data.Service
-// Author           : rapril
-// Created          : 06-30-2020
-//
-// Last Modified By : rapril
-// Last Modified On :
-// ***********************************************************************
-// <copyright file="ParseBasicTemplate.cs" company="Supply Side Data Management LLC">
-//     Copyright ©  2020 Supply Side Data Management. All rights reserved
-// </copyright>
-// <summary></summary>
-// ***********************************************************************
-
-
+﻿
 using EXCell.ConfigurationStore;
 using Microsoft.Extensions.Configuration;
 using System;
--- EXCell/Layouts/ILayout.cs
+++ EXCell/Layouts/ILayout.cs
@@ -1,16 +1,5 @@
 ﻿// ***********************************************************************
-// Assembly         : SSDMGMT.Data.Service
-// Author           : rapril
-// Created          : 06-30-2020
-//
-// Last Modified By : rapril
-// Last Modified On :
-// ***********************************************************************
-// <copyright file="ParseBasicTemplate.cs" company="Supply Side Data Management LLC">
-//     Copyright ©  2020 Supply Side Data Management. All rights reserved
-// </copyright>
-// <summary></summary>
-// ***********************************************************************
+
 
 using EXCell.ConfigurationStore;
 using EXCell.DataStructure;
